eb02668dd363e38d7536f0ab8f295408f3c35768
Nishant-kushwaha88 <nishantkumarkushwaha003@gmail.com>
2025-03-30 10:56:48 +0000

    dataTpes

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //We are using node.js, not browser
+
+let name="Nishant"
+let age=19;
+let isloggedIn=false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof null);
