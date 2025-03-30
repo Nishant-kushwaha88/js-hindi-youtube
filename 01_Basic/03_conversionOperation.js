2be5e356a1f60c93247251d2034a08b2c38524bc
Nishant-kushwaha88 <nishantkumarkushwaha003@gmail.com>
2025-03-30 12:13:55 +0000

    Operator

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -12,17 +12,59 @@
 // true => 1
 // false =>0
 
-let  isLoggedIn="";
+// let  isLoggedIn="";
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn);
 
 // 1 => true, 0 => false
 // "" => false
 //"Hello" =>true
 
-let someNumber=33;
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33;
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************* Operation ********************  //
+
+// let value=3;
+// let negvalue=-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3);
+
+// let str1="Hello"
+// let str2=" World"
+
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log(true);
+// console.log(+true);
+
+
+let num1, num2, num3
+
+num1=num2=num3=2+2
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
+
+// Link to study (Type coversion) =>
+  //  https://tc39.es/ecma262/#sec-abstract-operations
+
+
+// Link to study (x++, ++x) =>
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
 
