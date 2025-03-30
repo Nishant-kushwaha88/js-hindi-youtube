1c711c22b36253e998c8c8d32506109c0cae3045
Nishant-kushwaha88 <nishantkumarkushwaha003@gmail.com>
2025-03-30 12:41:39 +0000

    Comparision

---
+++ 01_Basic/04_comparision.js
@@ -0,0 +1,33 @@
+     // Basic comparision
+// console.log(2>1)
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==2);
+// console.log(2!=1);
+
+       //Strin to integer,boolean..
+// console.log("2">1);
+// console.log("02">0);
+
+      //null comparision
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+     
+       //undefined comparision
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+// ===
+       //triple comparision
+ console.log("2"===2);
+
+
+
+
+
+
+
+
