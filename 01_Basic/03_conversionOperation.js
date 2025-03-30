bf5b2a07ff6d6483b9d3c012bbaf7640a1b784c3
Nishant-kushwaha88 <nishantkumarkushwaha003@gmail.com>
2025-03-30 11:20:37 +0000

    Conersion of dataTypes

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score ="45abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN(Not a number)
+// true => 1
+// false =>0
+
+let  isLoggedIn="";
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+//"Hello" =>true
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
