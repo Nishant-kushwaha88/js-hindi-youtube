57c673ebe97236c0fdda927b31bc5e15250080be
Nishant-kushwaha88 <nishantkumarkushwaha003@gmail.com>
2025-03-31 23:42:08 +0000

    Nums and Math

---
+++ 01_Basic/06_nums_and_math.js
@@ -0,0 +1,18 @@
+const score=400
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const otherNumber=515.8966
+console.log(otherNumber.toPrecision(3));
+
+const hundred= 100000
+console.log(hundred.toLocaleString('en-IN'));
+
+
+
+
