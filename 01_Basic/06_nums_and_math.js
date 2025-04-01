82841f34f145dafef33d02ca7f7a76f2851c8137
Nishant-kushwaha88 <nishantkumarkushwaha003@gmail.com>
2025-04-01 09:07:42 +0000

    Nums and Math

--- 01_Basic/06_nums_and_math.js
+++ 01_Basic/06_nums_and_math.js
@@ -1,17 +1,45 @@
-const score=400
-console.log(score);
+// const score=400
+// console.log(score);
 
-const balance=new Number(100)
-console.log(balance);
+// const balance=new Number(100)
+// console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toFixed(2));
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
 
-const otherNumber=515.8966
-console.log(otherNumber.toPrecision(3));
+// const otherNumber=515.8966
+// console.log(otherNumber.toPrecision(3));
 
-const hundred= 100000
-console.log(hundred.toLocaleString('en-IN'));
+// const hundred= 100000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+//++++++++++++   MATH   +++++++++++++++++
+
+//console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(7.4));
+// console.log(Math.ceil(4.8));
+
+console.log(Math.random()); // ye 0 se lekar 1 ke bech ka numbr output me deta hai
+console.log((Math.random()*10) + 1);
+
+const min=10
+const max=20
+
+
+console.log(Math.random()* (max-min + 1))
+console.log(Math.floor(Math.random() * (max-min + 1)));
+
+
+
+
+
+
+
+
+
 
 
 
