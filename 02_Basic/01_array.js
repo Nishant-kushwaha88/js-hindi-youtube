0b17bbd0909e8bf78658a29a9fbdc8fd1da77fcf
Nishant-kushwaha88 <nishantkumarkushwaha003@gmail.com>
2025-04-01 11:39:15 +0000

    Array

---
+++ 02_Basic/01_array.js
@@ -0,0 +1,48 @@
+//  array
+
+const myArr=[0,1,2,3,4,5]
+
+//const myHeros=["Saktiman", "naagraj"]
+
+//const myArr2=new Array(1,2,3,4)
+
+// console.log(myArr[1]);
+
+// console.log(typeof myArr);
+
+// Array Method
+
+//myArr.push(7) //Add one value
+//myArr.pop()  // decress one value
+
+//myArr.unshift(9) //Add one value but yee first me add karte hai
+//myArr.shift(7)    // yee suru ke value ko hata deta hai
+
+// console.log(myArr.includes(9));   // ye pata karta hai ki arry me 9 hai ya nahi
+// console.log(myArr.indexOf(9));   //ye pata karta hai ki array me 6 index pe konsa value hai
+
+
+// const newArr=myArr.join();
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+///slice, splice [interview question]
+
+console.log("A", myArr);
+
+const myn1=myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2=myArr.splice(1,3)
+console.log(myn2);
+
+
+
+
+
+
+
