2c6612f4b71ddcbb1e0e844f563970fbe4e36385
Nishant-kushwaha88 <nishantkumarkushwaha003@gmail.com>
2025-04-05 11:18:31 +0000

    truth

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,54 @@
+const email="hitesh@.ai"
+// if(email){
+//     console.log("Yes");
+    
+// }else{
+//     console.log("Not email");
+    
+// }
+
+// Falsy values => false, 0, =0, BigInt 0n, "", null, undefined, NaN(Not a number)
+// Truth values => true, "0",'false', " ", [], {}, function(){}
+
+// const arr=[]
+// if (arr.length===0) {
+//     console.log("Yes");  
+//  }
+
+//  const emptyObj={}
+//  if (emptyObj) {
+//     console.log("Object is empty");
+    
+//  }
+
+ // Nullish coalescing Operator (??): underFined
+
+//  let val;
+
+//  val1= 5 ?? 10
+//  console.log(val1);
+ 
+//  val1=null ?? 10
+//  console.log(val1);
+ 
+//  val1=undefined ?? 15
+//  console.log(val1);
+
+//  val1=null ?? 20
+//  console.log(val1);
+
+
+ // Terniary Operator
+
+ // condition ? true: false 
+
+ const iceTeaPrice=100
+ iceTeaPrice <= 50 ? console.log("Less than 80") : console.log("more than 80");
+
+
+ 
+ 
+ 
+ 
+ 
+
